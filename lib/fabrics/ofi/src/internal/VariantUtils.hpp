b48378022d46fc96d515a03c9c79e5417eea4aef
Jonas Ohland <jonas.ohland@riedel.net>
2025-08-19 22:05:22 +0200

    Move "overloaded" template to ofi namespace
    Co-authored-by: Jonas Ohland <jonas.ohland@gmail.com>
    Signed-off-by: Jonas Ohland <jonas.ohland@riedel.net>

--- lib/fabrics/ofi/src/internal/VariantUtils.hpp
+++ lib/fabrics/ofi/src/internal/VariantUtils.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-// Allows us to use the visitor pattern with lambdas
-template<class... Ts>
-struct overloaded : Ts...
+namespace mxl::lib::fabrics::ofi
 {
-    using Ts::operator()...;
-};
+    // Allows us to use the visitor pattern with lambdas
+    template<class... Ts>
+    struct overloaded : Ts...
+    {
+        using Ts::operator()...;
+    };
+}
