736f7e1232626719b0c1ea02d2de843165f0629e
Jonas Ohland (Riedel) <jonas.ohland@riedel.net>
2026-02-11 18:22:57 +0100

    Handle 'validSlices' update internally
    Co-authored-by: Jonas Ohland <jonas.ohland@gmail.com>
    Signed-off-by: Jonas Ohland (Riedel) <jonas.ohland@riedel.net>

--- lib/internal/src/PosixContinuousFlowWriter.cpp
+++ lib/internal/src/PosixContinuousFlowWriter.cpp
@@ -28,6 +28,15 @@ namespace mxl::lib
         }
     }
 
+    FlowData& PosixContinuousFlowWriter::getFlowData()
+    {
+        if (_flowData)
+        {
+            return *_flowData;
+        }
+        throw std::runtime_error("No open flow.");
+    }
+
     FlowData const& PosixContinuousFlowWriter::getFlowData() const
     {
         if (_flowData)
